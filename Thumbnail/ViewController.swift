9ef74f93db5a37b3a36eb73c957567a1b2d9f848
Koray Birand <kbirand@mac.com>
2020-07-30 01:23:39 +0300

    Initial Commit

---
+++ Thumbnail/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Thumbnail
+//
+//  Created by Koray Birand on 30.07.2020.
+//  Copyright © 2020 Koray Birand. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
